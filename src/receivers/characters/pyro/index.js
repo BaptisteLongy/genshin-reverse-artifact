7abe83b2d5909dc56e9ef59837e5b7af71ea1582
Baptiste Longy <baptiste.longy@gmail.com>
2022-01-15 23:17:21 +0100

    feat: added Bennett

--- src/receivers/characters/pyro/index.js
+++ src/receivers/characters/pyro/index.js
@@ -1,7 +1,9 @@
 const amber = require('./amber.json')
 const xiangling = require('./xiangling.json')
+const bennett = require('./bennett.json')
 
 module.exports = [
     amber,
-    xiangling
+    xiangling,
+    bennett
 ];
