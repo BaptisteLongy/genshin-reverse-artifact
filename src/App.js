b09a14d2b72d35bc77bc754d79dd1ba51e2423af
Baptiste Longy <baptiste.longy@gmail.com>
2022-01-15 11:15:33 +0100

    chore: planned the main page

--- src/App.js
+++ src/App.js
@@ -4,20 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Genshin Reverse Artifact</h1>
+      <h2>Artifact section</h2>
+      <p>Here the user will be able to enter details of his artifact</p>
+      <h2>Character section</h2>
+      <p>Herer the user will be presented with the list of characters and ranking the artifact can be used with</p>
+      <h1>One day I will add a footer</h1>
     </div>
   );
 }
