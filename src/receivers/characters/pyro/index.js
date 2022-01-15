b3cb2c32ef3a344c7de9d6fccfd22e1b0459107c
Baptiste Longy <baptiste.longy@gmail.com>
2022-01-15 23:29:13 +0100

    feat: added Xinyan

--- src/receivers/characters/pyro/index.js
+++ src/receivers/characters/pyro/index.js
@@ -1,9 +1,11 @@
 const amber = require('./amber.json')
 const xiangling = require('./xiangling.json')
 const bennett = require('./bennett.json')
+const xinyan = require('./xinyan.json')
 
 module.exports = [
     amber,
     xiangling,
-    bennett
+    bennett,
+    xinyan
 ];
