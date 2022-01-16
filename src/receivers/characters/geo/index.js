eecaa509f3465c9103cdaab0ce76f15f6aa0dce6
Baptiste Longy <baptiste.longy@gmail.com>
2022-01-16 13:09:57 +0100

    feat: added You Jin

--- src/receivers/characters/geo/index.js
+++ src/receivers/characters/geo/index.js
@@ -1,9 +1,11 @@
 const ningguang = require('./ningguang.json')
 const noelle = require('./noelle.json')
 const gorou = require('./gorou.json')
+const you_jin = require('./you jin.json')
 
 module.exports = [
     ningguang,
     noelle,
-    gorou
+    gorou,
+    you_jin
 ];
