f7793b59c78a79ce4e801e1e9508fba9e983f484
Baptiste Longy <baptiste.longy@gmail.com>
2022-01-16 13:06:51 +0100

    feat: added Gorou

--- src/receivers/characters/geo/index.js
+++ src/receivers/characters/geo/index.js
@@ -1,7 +1,9 @@
 const ningguang = require('./ningguang.json')
 const noelle = require('./noelle.json')
+const gorou = require('./gorou.json')
 
 module.exports = [
     ningguang,
-    noelle
+    noelle,
+    gorou
 ];
