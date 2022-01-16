978315a84c1a7cace1435baf8e695218c11bc625
Baptiste Longy <baptiste.longy@gmail.com>
2022-01-16 09:34:24 +0100

    feat: added Yanfei

--- src/receivers/characters/pyro/index.js
+++ src/receivers/characters/pyro/index.js
@@ -2,10 +2,12 @@ const amber = require('./amber.json')
 const xiangling = require('./xiangling.json')
 const bennett = require('./bennett.json')
 const xinyan = require('./xinyan.json')
+const yanfei = require('./yanfei.json')
 
 module.exports = [
     amber,
     xiangling,
     bennett,
-    xinyan
+    xinyan,
+    yanfei
 ];
