f85090c618558e027ea5c442993284ec0236afab
Baptiste Longy <baptiste.longy@gmail.com>
2022-01-16 10:12:57 +0100

    feat: adde Kujou Sara

--- src/receivers/characters/electro/index.js
+++ src/receivers/characters/electro/index.js
@@ -2,10 +2,13 @@ const fischl = require('./fischl.json')
 const beidou = require('./beidou.json')
 const lisa = require('./lisa.json')
 const razor = require('./razor.json')
+const kujou_sara = require('./kujou sara.json')
+
 
 module.exports = [
     fischl,
     beidou,
     lisa,
-    razor
+    razor,
+    kujou_sara
 ];
