07f0edc76659979d6815810e309e3e5c9d850f52
Baptiste Longy <baptiste.longy@gmail.com>
2022-01-16 12:47:56 +0100

    feat: added Xiao

--- src/receivers/characters/anemo/index.js
+++ src/receivers/characters/anemo/index.js
@@ -3,11 +3,13 @@ const sayu = require('./sayu.json')
 const anemo_traveler = require('./anemo traveler.json')
 const jean = require('./jean.json')
 const venti = require('./venti.json')
+const xiao = require('./xiao.json')
 
 module.exports = [
     sucrose,
     sayu,
     anemo_traveler,
     jean,
-    venti
+    venti,
+    xiao
 ];
