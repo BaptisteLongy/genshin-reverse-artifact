e8918ce175d138876236e8575aadc0fcc219a408
Baptiste Longy <baptiste.longy@gmail.com>
2022-01-16 10:45:13 +0100

    feat: added Childe

--- src/receivers/characters/hydro/index.js
+++ src/receivers/characters/hydro/index.js
@@ -1,7 +1,9 @@
 const xingqiu = require('./xingqiu.json')
 const barbara = require('./barbara.json')
+const childe = require('./childe.json')
 
 module.exports = [
     xingqiu,
-    barbara
+    barbara,
+    childe
 ];
