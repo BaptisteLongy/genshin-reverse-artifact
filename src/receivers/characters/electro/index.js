c7043231ae3ae4963f5a5683cc2cc877aa64944c
Baptiste Longy <baptiste.longy@gmail.com>
2022-01-16 10:09:54 +0100

    feat: added Razor

--- src/receivers/characters/electro/index.js
+++ src/receivers/characters/electro/index.js
@@ -1,9 +1,11 @@
 const fischl = require('./fischl.json')
 const beidou = require('./beidou.json')
 const lisa = require('./lisa.json')
+const razor = require('./razor.json')
 
 module.exports = [
     fischl,
     beidou,
-    lisa
+    lisa,
+    razor
 ];
