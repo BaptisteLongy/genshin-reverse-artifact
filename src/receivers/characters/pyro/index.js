661a4ebf1a384d8ef6ef2bc590cc3db5c37b45dc
Baptiste Longy <baptiste.longy@gmail.com>
2022-01-16 09:35:03 +0100

    feat: added Thoma

--- src/receivers/characters/pyro/index.js
+++ src/receivers/characters/pyro/index.js
@@ -3,11 +3,13 @@ const xiangling = require('./xiangling.json')
 const bennett = require('./bennett.json')
 const xinyan = require('./xinyan.json')
 const yanfei = require('./yanfei.json')
+const thoma = require('./thoma.json')
 
 module.exports = [
     amber,
     xiangling,
     bennett,
     xinyan,
-    yanfei
+    yanfei,
+    thoma
 ];
