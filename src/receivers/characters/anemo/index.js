abcf26b5c6c6ceb152cb8aaef37ed28ba1dfc29e
Baptiste Longy <baptiste.longy@gmail.com>
2022-01-16 12:43:19 +0100

    feat: added Jean

--- src/receivers/characters/anemo/index.js
+++ src/receivers/characters/anemo/index.js
@@ -1,9 +1,11 @@
 const sucrose = require('./sucrose.json')
 const sayu = require('./sayu.json')
 const anemo_traveler = require('./anemo traveler.json')
+const jean = require('./jean.json')
 
 module.exports = [
     sucrose,
     sayu,
-    anemo_traveler
+    anemo_traveler,
+    jean
 ];
