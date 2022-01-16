5dd3b62abf9c6b222120eff26b4969ad413979b0
Baptiste Longy <baptiste.longy@gmail.com>
2022-01-16 11:35:39 +0100

    feat: added Rosaria

--- src/receivers/characters/cryo/index.js
+++ src/receivers/characters/cryo/index.js
@@ -1,9 +1,11 @@
 const diona = require('./diona.json')
 const chongyun = require('./chongyun.json')
 const kaeya = require('./kaeya.json')
+const rosaria = require('./rosaria.json')
 
 module.exports = [
     diona,
     chongyun,
-    kaeya
+    kaeya,
+    rosaria
 ];
