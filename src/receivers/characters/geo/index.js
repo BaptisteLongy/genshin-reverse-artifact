0a0a93ccb315348df82dc1b03e29d21562487ecd
Baptiste Longy <baptiste.longy@gmail.com>
2022-01-16 13:16:25 +0100

    feat: added Geo Traveler

--- src/receivers/characters/geo/index.js
+++ src/receivers/characters/geo/index.js
@@ -2,10 +2,12 @@ const ningguang = require('./ningguang.json')
 const noelle = require('./noelle.json')
 const gorou = require('./gorou.json')
 const you_jin = require('./you jin.json')
+const geo_traveler = require('./geo traveler.json')
 
 module.exports = [
     ningguang,
     noelle,
     gorou,
-    you_jin
+    you_jin,
+    geo_traveler
 ];
