9a3bb7e9d7e18d7fce8ab572fec7140eb4b47ab0
Baptiste Longy <baptiste.longy@gmail.com>
2022-01-16 10:06:41 +0100

    feat: added Lisa

--- src/receivers/characters/electro/index.js
+++ src/receivers/characters/electro/index.js
@@ -1,7 +1,9 @@
 const fischl = require('./fischl.json')
 const beidou = require('./beidou.json')
+const lisa = require('./lisa.json')
 
 module.exports = [
     fischl,
-    beidou
+    beidou,
+    lisa
 ];
