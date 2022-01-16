4b2373bccadd4a009431b218196faf8b5873d599
Baptiste Longy <baptiste.longy@gmail.com>
2022-01-16 10:59:55 +0100

    feat: added Kokomi

--- src/receivers/characters/hydro/index.js
+++ src/receivers/characters/hydro/index.js
@@ -2,10 +2,12 @@ const xingqiu = require('./xingqiu.json')
 const barbara = require('./barbara.json')
 const childe = require('./childe.json')
 const mona = require('./mona.json')
+const kokomi = require('./kokomi.json')
 
 module.exports = [
     xingqiu,
     barbara,
     childe,
-    mona
+    mona,
+    kokomi
 ];
