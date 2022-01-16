4277cf730eea436f966192417f5899512293bf95
Baptiste Longy <baptiste.longy@gmail.com>
2022-01-16 11:40:35 +0100

    feat: added Qiqi

--- src/receivers/characters/cryo/index.js
+++ src/receivers/characters/cryo/index.js
@@ -2,10 +2,12 @@ const diona = require('./diona.json')
 const chongyun = require('./chongyun.json')
 const kaeya = require('./kaeya.json')
 const rosaria = require('./rosaria.json')
+const qiqi = require('./qiqi.json')
 
 module.exports = [
     diona,
     chongyun,
     kaeya,
-    rosaria
+    rosaria,
+    qiqi
 ];
