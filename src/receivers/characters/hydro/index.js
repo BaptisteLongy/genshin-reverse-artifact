beec8f16d74409c2808b28e2007b7901e2bb73cc
Baptiste Longy <baptiste.longy@gmail.com>
2022-01-16 10:47:43 +0100

    feat: added Mona

--- src/receivers/characters/hydro/index.js
+++ src/receivers/characters/hydro/index.js
@@ -1,9 +1,11 @@
 const xingqiu = require('./xingqiu.json')
 const barbara = require('./barbara.json')
 const childe = require('./childe.json')
+const mona = require('./mona.json')
 
 module.exports = [
     xingqiu,
     barbara,
-    childe
+    childe,
+    mona
 ];
