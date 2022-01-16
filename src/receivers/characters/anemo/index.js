b71877668b8edc425a77871eae4b93b3b92880db
Baptiste Longy <baptiste.longy@gmail.com>
2022-01-16 12:46:00 +0100

    feat: added Venti

--- src/receivers/characters/anemo/index.js
+++ src/receivers/characters/anemo/index.js
@@ -2,10 +2,12 @@ const sucrose = require('./sucrose.json')
 const sayu = require('./sayu.json')
 const anemo_traveler = require('./anemo traveler.json')
 const jean = require('./jean.json')
+const venti = require('./venti.json')
 
 module.exports = [
     sucrose,
     sayu,
     anemo_traveler,
-    jean
+    jean,
+    venti
 ];
