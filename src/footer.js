07bed4135e98f482339814fee030b1defb2b6a21
Baptiste Longy <baptiste.longy@gmail.com>
2022-02-04 18:21:59 +0100

    chore: changed last updated date

--- src/footer.js
+++ src/footer.js
@@ -20,7 +20,7 @@ export default function Footer() {
                     <br />
                     Data is coming from <a href="https://docs.google.com/spreadsheets/u/1/d/1gNxZ2xab1J6o1TuNVWMeLOZ7TPOqrsf3SshP5DLvKzI/htmlview?pru=AAABd4N-fLk*X-bMgWoyQtpHPIS7FN1hTQ#">Genshin Impact Helper Team's Character Builds</a>
                     <br />
-                    Last updated on 01/16/2021
+                    Last updated on 02/04/2021
                 </Grid>
                 <Grid item >
                     <IconButton aria-label="GitHub.com" onClick={() => window.open('https://github.com/BaptisteLongy/genshin-reverse-artifact', "_blank")}>
